a540d3e7ab7e1c5352a320c7745e5025f81d4542
justastranger <justastrangerlica@gmail.com>
2022-01-01 03:58:15 -0600

    Some cleanup Comments Re-enable Harmony patch Add debug mode logging

--- ThunderstormTotem/ModEntry.cs
+++ ThunderstormTotem/ModEntry.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using StardewValley;
+﻿using StardewValley;
 using StardewModdingAPI;
 using HarmonyLib;
 using StardewModdingAPI.Events;
@@ -15,6 +14,7 @@ namespace ThunderstormTotem
         public static IMonitor Logger;
         public static IModHelper ModHelper;
         internal Config config;
+        public static bool debugMode;
         internal Harmony Harmony = new("jas.ThunderstormTotem");
 
         public override void Entry(IModHelper helper)
@@ -24,39 +24,37 @@ namespace ThunderstormTotem
             ModHelper = helper;
             Helper.Events.Display.MenuChanged += OnMenuChanged;
             config = helper.ReadConfig<Config>();
-            // Harmony.Patch(
-            //     original: AccessTools.Method(typeof(Object), "rainTotem"),
-            //     prefix: new HarmonyMethod(AccessTools.Method(typeof(RainTotemPatch), "RainTotemPrefix"))
-            // );
-            // Logger.Log("Patched Object::rainTotem");
+            debugMode = config.Debug;
+            Harmony.Patch(
+                original: AccessTools.Method(typeof(Object), "rainTotem"),
+                prefix: new HarmonyMethod(AccessTools.Method(typeof(RainTotemPatch), "RainTotemPrefix"))
+            );
+            Logger.Log("Patched Object::rainTotem");
         }
 
         public void OnMenuChanged(object sender, MenuChangedEventArgs e)
         {
+            // Check whether the player's holding something and whether that something is a Rain Totem
             // 681 is the ParentSheetIndex for the Rain Totem. There's no const for it though.
             if (Game1.player.ActiveObject != null && Game1.player.ActiveObject.ParentSheetIndex == 681)
             {
+                // If the player's holding something and the new menu is a DialogueBox
                 if (e.NewMenu is DialogueBox box)
                 {
-                    List<string> lines = box.dialogues;
-                    if (lines != null &&
-                        lines.Count == 1 &&
-                        lines[0] == Game1.content.LoadString("Strings\\StringsFromCSFiles:Object.cs.12822") &&
+                    if (box.dialogues != null &&
+                        box.dialogues.Count == 1 &&
+                        box.dialogues[0] == Game1.content.LoadString("Strings\\StringsFromCSFiles:Object.cs.12822") &&
                         AlreadyRaining)
                     {
-                        // Rain Totem DialogueBox
-                        lines.Clear();
-                        lines.Add(I18N.Get("ThunderstormTotem.RainToThunder"));
+                        // code below only runs if it's the Rain Totem DialogueBox and AlreadyRaining is flagged as true
+                        // AlreadyRaining is updated each time the Rain Totem is used, before it's actually used
+                        if (debugMode) Logger.Log("Stormy Weather Conjured.");
+                        box.dialogues.Clear();
+                        box.dialogues.Add(I18N.Get("ThunderstormTotem.RainToThunder"));
                         Game1.netWorldState.Value.WeatherForTomorrow = Game1.weatherForTomorrow = Game1.weather_lightning;
                     }
                 }
             }
-        }
-
-        private void ConjureStormyWeather()
-        {
-            Logger.Log("Stormy Weather Conjured.");
-            
         }
     }
 
@@ -66,7 +64,7 @@ namespace ThunderstormTotem
 
         private static bool RainTotemPrefix(Item __instance, Farmer who)
         {
-            ModEntry.Logger.Log("RainTotemPrefix executed.");
+            if (ModEntry.debugMode) ModEntry.Logger.Log("RainTotemPrefix executed.");
             var weather = Game1.netWorldState.Value.GetWeatherForLocation(Game1.currentLocation.GetLocationContext()).weatherForTomorrow.Value;
 
             // If tomorrow is a festival day or if the weather for tomorrow is not going to be rain, set AlreadyRaining to false
@@ -78,7 +76,8 @@ namespace ThunderstormTotem
             {
                 ModEntry.AlreadyRaining = true;
             }
-            
+
+            if (ModEntry.debugMode) ModEntry.Logger.Log("AlreadyRaining set to: " + ModEntry.AlreadyRaining.ToString());
             return true;
         }
     }
